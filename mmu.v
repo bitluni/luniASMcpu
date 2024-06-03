07a8e5a1dea84abac4c7450db0eb5d053a9bf978
Lanea Lucy S <lanealucy@gmail.com>
2024-06-04 01:25:48 +0200

    repaired mmu; changed debug led functions

--- mmu.v
+++ mmu.v
@@ -18,15 +18,15 @@ reg [7:0] rom[255:0];
 
 initial begin
 	//$readmemh("rom.mem", rom, 0);
-	rom[0] = 8'hff; //8'h05;
-	rom[1] = 8'hff; //8'hBA;
-	rom[2] = 8'hff; //8'hAD;
-	rom[3] = 8'hff; //8'h05;
-	rom[4] = 8'hff; //8'hF0;
-	rom[5] = 8'hff; //8'h0D;
-	rom[6] = 8'h10;
-	rom[7] = 8'h00;
-	rom[8] = 8'h00;
+	rom[0] = 8'd255; //8'h05;
+	rom[1] = 8'd254; //8'hBA;
+	rom[2] = 8'd255; //8'hAD;
+	rom[3] = 8'd254; //8'h05;
+	rom[4] = 8'd255; //8'hF0;
+	rom[5] = 8'd254; //8'h0D;
+	rom[6] = 8'd16;
+	rom[7] = 8'd16;
+	rom[8] = 8'd16;
 	rom[9] = 8'h00;
 	rom[10] = 8'h00;
 	rom[11] = 8'h00;
@@ -41,14 +41,14 @@ reg readActive;
 reg writeActive;
 
 always @(posedge read) begin
-	byteNumber <= 0;
-	readActive <= 1;
-	dataOut <= 0;
+//	byteNumber <= 0;
+//	readActive <= 1;
+//	dataOut <= 0;
 end
 
 always @(posedge write) begin
-	byteNumber <= 0;
-	writeActive <= 1;
+//	byteNumber <= 0;
+//	writeActive <= 1;
 end
 
 always @(posedge clk) begin
@@ -65,34 +65,37 @@ always @(posedge clk) begin
 			if(read) begin
 			 	if(readActive) begin
 					if(!address[8]) begin
-						dataOut <= dataOut | (sram[address[7:0] + byteNumber] << byteNumber * 8);
+						dataOut <= (dataOut << (byteNumber * 8)) | (sram[address[7:0] + byteNumber]);
 					end else begin
-						dataOut <= dataOut | (rom[address[7:0] + byteNumber] << byteNumber * 8);
+						dataOut <= (dataOut << (byteNumber * 8)) | (rom[address[7:0] + byteNumber]);
 					end
 					if(byteNumber != byteCount) begin
 						byteNumber <= byteNumber + 1;
 					end else begin
-						byteNumber <= 0; //optional
+						byteNumber <= 0;
 						dataOutReady <= 1'b1;
 						readActive <= 1'b0;
 					end
 				end else begin
+                    readActive <= 1'b1;
+                    dataOut <= 0;
 				end
 			end else if (write) begin
 			 	if(writeActive) begin
 					if(!address[8]) begin	
-						sram[address[7:0] + byteNumber]	<= (dataIn >> byteNumber * 8);
+						sram[address[7:0] + byteNumber]	<= (dataIn >> (byteNumber * 8));
 					end else begin
 						//rom read only
 					end
 					if(byteNumber != byteCount) begin
 						byteNumber <= byteNumber + 1;
 					end else begin
-						byteNumber <= 0; //optional
+						byteNumber <= 0;
 						dataInReady <= 1'b1;
 						writeActive <= 1'b0;
 					end
 				end else begin
+                    writeActive <= 1'b1;
 				end
 			end 
 		end
